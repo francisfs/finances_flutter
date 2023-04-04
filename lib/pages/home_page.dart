34e18f3b63507ea1e46f5bba39eb46d373832e27
FRANCISLEY <55454628+francisfs@users.noreply.github.com>
2023-04-04 17:55:59 -0300

    listview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,17 @@
 import 'package:finances_flutter/constants.dart';
 import 'package:finances_flutter/data/data.dart';
+import 'package:finances_flutter/models/cost_model.dart';
+import 'package:finances_flutter/models/type_model.dart';
 import 'package:finances_flutter/widgets/custon_chart.dart';
 import 'package:finances_flutter/widgets/icon_btn.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
+
+//https://www.youtube.com/watch?v=MJFz6K8lXyA&list=PPSV
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -48,22 +53,72 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
           SliverList(
-            delegate: SliverChildBuilderDelegate(
-              //childCount: 1,
-              (context, index) => Container(
-                margin: EdgeInsets.only(
-                    left: 2.w, right: 2.w, top: 2.h, bottom: 2.h),
-                decoration: BoxDecoration(
-                  color: kPrimariColor,
-                  borderRadius: BorderRadius.circular(3.h),
+            delegate:
+                SliverChildBuilderDelegate(childCount: 1, (context, index) {
+              if (index == 0) {
+                return Container(
+                  margin: EdgeInsets.only(
+                      left: 2.w, right: 2.w, top: 2.h, bottom: 2.h),
+                  decoration: BoxDecoration(
+                    color: kPrimariColor,
+                    borderRadius: BorderRadius.circular(3.h),
+                  ),
+                  child: CustonChart(
+                    expenses: weeklySpending,
+                  ),
+                );
+              } else {
+                final TypeModel typeModel = typeNames[index - 1];
+                double tAmountSpend = 1;
+                typeModel.expenses!.forEach((CostModel expense) {
+                  tAmountSpend += expense.cost;
+                });
+                return _buildCategories(typeModel, tAmountSpend);
+              }
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildCategories(TypeModel category, double tAmountSpend) {
+    return Container(
+      width: 100.w,
+      height: 13.h,
+      margin: EdgeInsets.symmetric(horizontal: 2.w, vertical: 1.h),
+      padding: EdgeInsets.all(2.h),
+      decoration: BoxDecoration(
+        color: kPrimariColor,
+        borderRadius: BorderRadius.circular(2.h),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                category.name!,
+                style: GoogleFonts.abel(
+                  fontSize: 14.sp,
+                  color: kTextColor,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 1.0,
                 ),
-                child: CustonChart(expenses: weeklySpending,),
               ),
-            ),
+              Text(
+                '\$${(category.maxAmount! - tAmountSpend).toStringAsFixed(2)} / \$${category.maxAmount!.toStringAsFixed(2)}',
+                style: GoogleFonts.atma(
+                  fontSize: 14.sp,
+                  color: kTextColor,
+                  fontWeight: FontWeight.w500,
+                  letterSpacing: 1.0,
+                ),
+              ),
+            ],
           ),
         ],
       ),
     );
   }
 }
-
