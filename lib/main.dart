0963c3922a9b5c2308c11a5e52f8253dd37a1ea7
FRANCISLEY <55454628+francisfs@users.noreply.github.com>
2023-04-04 12:35:03 -0300

    lista

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'constants.dart';
 
 import 'pages/home_page.dart';
@@ -12,60 +13,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Expense Manager',
-      theme: ThemeData(
-        scaffoldBackgroundColor: kScafoldColor
-        
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+    return Sizer(builder: (context, Orientation, DeviceType) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Expense Manager',
+        theme: ThemeData(
+            scaffoldBackgroundColor: kScafoldColor,
+            appBarTheme: AppBarTheme(
+              backgroundColor: kPrimariColor,
+              iconTheme: IconThemeData(size: 23, color: kSecundaryColor),
+            ),
+            iconTheme: IconThemeData(size: 23, color: kSecundaryColor)),
+        home: const HomePage(),
+      );
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
+import 'package:finances_flutter/constants.dart';
+import 'package:finances_flutter/widgets/icon_btn.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,11 +20,77 @@ class _HomePageState extends State<HomePage> {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            leading: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.settings_outlined),
+            forceElevated: true,
+            floating: true,
+            expandedHeight: 10.h,
+            leading: CustonBtn(
+              onPress: () {},
+              iconData: Icons.settings_outlined,
             ),
-          )
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(
+                'My Buget',
+                style: GoogleFonts.aBeeZee(
+                    fontSize: 12.sp,
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.w500,
+                    color: kTextColor),
+              ),
+            ),
+            actions: [
+              CustonBtn(
+                onPress: () {},
+                iconData: Icons.add_outlined,
+              ),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              //childCount: 1,
+              (context, index) => Container(
+                margin: EdgeInsets.only(
+                    left: 2.w, right: 2.w, top: 2.h, bottom: 2.h),
+                decoration: BoxDecoration(
+                    color: kPrimariColor,
+                    borderRadius: BorderRadius.circular(3.h)),
+                child: Column(
+                  children: [
+                    Text(
+                      'Weekly Spendin',
+                      style: GoogleFonts.abel(
+                        fontSize: 13.sp,
+                        fontWeight: FontWeight.w600,
+                        color: kTextColor,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 1.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CustonBtn(
+                            onPress: () {},
+                            iconData: Icons.arrow_back_outlined),
+                        Text(
+                          'Nov 10, 2020 - Nov 18, 2021',
+                          style: GoogleFonts.atma(
+                              fontSize: 13.sp,
+                              fontWeight: FontWeight.w600,
+                              color: kTextColor),
+                        ),
+                        CustonBtn(
+                            onPress: () {},
+                            iconData: Icons.arrow_forward_outlined),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
